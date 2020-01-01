d991cccace77cc4adabce71baed1e79670bf7c91
钱宁博 <1072765270@qq.com>
2020-01-01 16:17:12 +0800

    添加串口例程

---
+++ cube_demo_usart/MDK-ARM/RTE/_cube_demo_usart/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'cube_demo_usart' 
+ * Target:  'cube_demo_usart' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
